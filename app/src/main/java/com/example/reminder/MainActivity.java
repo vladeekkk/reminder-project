fb45cacc1d733ff9ab808283186edbe6a60a7c9e
vladeekkk <vlad.krasss@yandex.ru>
2021-03-09 17:49:22 +0300

    init commit

---
+++ app/src/main/java/com/example/reminder/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView textView;
+    private EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.text_view);
+        editText = findViewById(R.id.edit_text);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                textView.setText("Hello, " + name + "!");
+            }
+        });
+
+
+    }
+}
