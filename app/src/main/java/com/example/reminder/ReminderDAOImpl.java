ca6177a46209355f977f7306a63c061d711a4037
Kira5005-code <anisimova5005@gmail.com>
2021-03-11 22:17:20 +0300

    add Reminder and ReminderDAO

---
+++ app/src/main/java/com/example/reminder/ReminderDAOImpl.java
@@ -0,0 +1,25 @@
+package main.java.com.example.reminder;
+
+import java.util.List;
+
+public class ReminderDAOImpl implements ReminderDAO{
+    @Override
+    public void save(Reminder reminder) {
+
+    }
+
+    @Override
+    public void update(Reminder reminder) {
+
+    }
+
+    @Override
+    public void delete(Reminder reminder) {
+
+    }
+
+    @Override
+    public List<Reminder> findAll() {
+        return null;
+    }
+}
