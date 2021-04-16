b5dcb7d1705956ccb96f07f5edad47286bb89c17
vladeekkk <vlad.krasss@yandex.ru>
2021-04-16 17:50:30 +0300

    added push notifications

--- app/src/main/java/com/example/reminder/MainActivity.java
+++ app/src/main/java/com/example/reminder/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +24,17 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private EditText editText;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reminder_item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // examle of pushing a notification
+        PushReminderImpl reminderPusher = new PushReminderImpl(this);
+        reminderPusher.push(new Reminder(0, "03.11.2001", "homework"));
     }
 }
