6fbe4ecc20f9a8bde48182b466aba693dd571296
vladeekkk <vlad.krasss@yandex.ru>
2021-05-22 00:49:17 +0300

    added listview dividor

--- app/src/main/java/com/example/reminder/AllRemindersView.java
+++ app/src/main/java/com/example/reminder/AllRemindersView.java
@@ -33,12 +33,12 @@ public class AllRemindersView extends AppCompatActivity {
 
         adapter = new ArrayAdapter<String>(
                 this,
-                android.R.layout.simple_list_item_1,
+                R.layout.simple_list_item,
                 listOfReminders);
 
         adapterAllInformation = new ArrayAdapter<String>(
                 this,
-                android.R.layout.simple_list_item_1,
+                R.layout.simple_list_item,
                 listOfRemindersAllInformation);
 
 
